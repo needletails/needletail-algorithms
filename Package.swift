ae635e49a389185d89dcad5c9dfd2f2e6dd64377
Cole Moore <Cartisim@icloud.com>
2024-04-26 19:32:40 +0800

    using NeedleTailLogger 1.0.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-atomics.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.65.0")),
         .package(url: "https://github.com/orlandos-nl/BSON.git", from: "8.1.0"),
-        .package(url: "git@github.com:needle-tail/needletail-logger.git", branch: "main")
+        .package(url: "git@github.com:needle-tail/needletail-logger.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
